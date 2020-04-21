eac51743f6b5edf6bb25a1830f601962954537c2
Eryk <er713@eryk-Latitude.home>
2020-04-21 15:16:51 +0200

    sprawozdanie z eksperymentem

--- Python/tests.py
+++ Python/tests.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         rev, rmse = tomograf.do_all(image, 180, 180, 180, True)
         res.write(str(rmse) + "\n\n")
         imsave("wynik/default.jpg", rev)
-
+    #
     with open("wynik/decoders.txt", 'w') as res:
         res.write("Dekodery:\n")
         for decoders in range(90, 721, 90):
@@ -17,12 +17,12 @@ if __name__ == "__main__":
             rev, rmse = tomograf.do_all(image, decoders, 180, 180, True)
             res.write("( " + str(decoders) + ", " + str(rmse) + ")\n")
             imsave("wynik/dec" + str(decoders) + ".jpg", rev)
-
+    #
     with open("wynik/step.txt", 'w') as res:
         res.write("\nScany:\n")
         for steps in range(90, 721, 90):
             tomograf = Transform()
-            rev, rmse = tomograf.do_all(image, 180, steps, 180, True)
+            rev, rmse = tomograf.do_all(image, 180, 180, steps, True)
             res.write("( " + str(steps) + ", " + str(rmse) + ")\n")
             imsave("wynik/step" + str(steps) + ".jpg", rev)
 
@@ -30,10 +30,10 @@ if __name__ == "__main__":
         res.write("\nRozpietosc:\n")
         for rang in range(45, 271, 45):
             tomograf = Transform()
-            rev, rmse = tomograf.do_all(image, 180, 180, rang, True)
+            rev, rmse = tomograf.do_all(image, 180, rang, 180, True)
             res.write("( " + str(rang) + ", " + str(rmse) + ")\n")
             imsave("wynik/rang" + str(rang) + ".jpg", rev)
-
+    #
     with open("wynik/pic1.txt", 'w') as res:
         res.write("\nObraz 1:\n")
         image = imread("zdjecia/SADDLE_PE.JPG", True)
@@ -43,7 +43,7 @@ if __name__ == "__main__":
         rev, rmse = Transform().do_all(image, 360, 270, 360, True)
         res.write(str(rmse)+"\n")
         imsave("wynik/o1_zf.jpg", rev)
-
+    #
     with open("wynik/pic2.txt", 'w') as res:
         res.write("\nObraz 2:\n")
         image = imread("zdjecia/CT_ScoutView.jpg", True)
